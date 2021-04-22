06b1c06e688efb2e06a383df7e8a691eecead97e
Hritik Soni <hritik.s@greyorange.com>
2021-04-22 16:14:40 +0530

    making healthmon components configurable

--- src/healthmon_sup.erl
+++ src/healthmon_sup.erl
@@ -8,17 +8,52 @@ start_link() ->
 	supervisor3:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-	AppmonWatcher = {appmon_watcher, {appmon_watcher, start_link, []},
-			{permanent, 30}, 2000, worker, [appmon_watcher]},
-	HealthMon = {healthmon, {healthmon, start_link, []},
-			{permanent, 30}, 2000, worker, [healthmon]},
-	HealthMonitor  = {health_monitor, {health_monitor, start_link, []},
-		permanent, 2000, worker, [health_monitor]},
-	HealthmonInstrumenter  = {healthmon_instrumenter, {healthmon_instrumenter, start_link, []},
-		{permanent, 30}, 2000, worker, [healthmon_instrumenter]},
-	CrashMonitorSup = {crash_monitor_watcher_sup, {crash_monitor_watcher_sup, start_link, []},
-		temporary, infinity, supervisor, [crash_monitor_watcher_sup]},
-	Procs = [AppmonWatcher, HealthMon, HealthMonitor, CrashMonitorSup, HealthmonInstrumenter],
-	{ok, {{one_for_one, 1, 5}, Procs}}.
+	{ok, {{one_for_one, 1, 5}, get_enabled_children_specs()}}.
 
 post_init([]) -> ignore.
+
+get_healthmon_components() ->
+	[healthmon_core, starvmon, crashmon, healthmon_instrumenter].
+
+get_enabled_children_specs() ->
+	get_enabled_children_specs(get_healthmon_components(), []).
+
+get_enabled_children_specs([], Acc) -> Acc;
+
+get_enabled_children_specs([healthmon_core | Rest], Acc) ->
+	case application:get_env(healthmon, healthmon_core_enabled, false) of
+		true ->
+			AppmonWatcher = {appmon_watcher, {appmon_watcher, start_link, []},
+				{permanent, 30}, 2000, worker, [appmon_watcher]},
+			HealthMon = {healthmon, {healthmon, start_link, []},
+				{permanent, 30}, 2000, worker, [healthmon]},
+			get_enabled_children_specs(Rest, Acc ++ [AppmonWatcher, HealthMon]);
+		false -> get_enabled_children_specs(Rest, Acc)
+	end;
+
+get_enabled_children_specs([starvmon | Rest], Acc) ->
+	case application:get_env(healthmon, starvmon_enabled, true) of
+		true ->
+			HealthMonitor  = {health_monitor, {health_monitor, start_link, []},
+				permanent, 2000, worker, [health_monitor]},
+			get_enabled_children_specs(Rest, Acc ++ [HealthMonitor]);
+		false -> get_enabled_children_specs(Rest, Acc)
+	end;
+
+get_enabled_children_specs([crashmon | Rest], Acc) ->
+	case application:get_env(healthmon, crashmon_enabled, true) of
+		true ->
+			CrashMonitorSup = {crash_monitor_watcher_sup, {crash_monitor_watcher_sup, start_link, []},
+				temporary, infinity, supervisor, [crash_monitor_watcher_sup]},
+			get_enabled_children_specs(Rest, Acc ++ [CrashMonitorSup]);
+		false -> get_enabled_children_specs(Rest, Acc)
+	end;
+
+get_enabled_children_specs([healthmon_instrumenter | Rest], Acc) ->
+	case application:get_env(healthmon, healthmon_instrumenter_enabled, true) of
+		true ->
+			HealthmonInstrumenter  = {healthmon_instrumenter, {healthmon_instrumenter, start_link, []},
+				{permanent, 30}, 2000, worker, [healthmon_instrumenter]},
+			get_enabled_children_specs(Rest, Acc ++ [HealthmonInstrumenter]);
+		false -> get_enabled_children_specs(Rest, Acc)
+	end.
