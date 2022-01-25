f13263e0cc7651393632d196799a9e4ccfb144ab
Hritik Soni <hritik.s@greyorange.com>
2022-01-25 08:57:40 +0000

    correcting app.src file

--- src/healthmon_app.erl
+++ src/healthmon_app.erl
@@ -1,8 +1,11 @@
 -module(healthmon_app).
 -behaviour(application).
 
--export([start/2]).
+-export([start/2, start/0]).
 -export([stop/1]).
+
+start() ->
+	healthmon_sup:start_link().
 
 start(_Type, _Args) ->
 	healthmon_sup:start_link().
